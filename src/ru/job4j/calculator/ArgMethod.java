143550abf27e72415bb5132e725b787fbfc19fb8
dima- <dmitry.yamanko@gmail.com>
2021-03-08 12:33:37 +0100

    4.1. Аргументы [#489586]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name, int age){
+        System.out.println("Hello, " + name + "\nand I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        String name = "Dima Yamanko";
+        int age = 31;
+
+       // ArgMethod.hello();
+        ArgMethod.hello(name, age);
+//        ArgMethod.hello(age, age);
+//        ArgMethod.hello(name, name, name);
+    }
+}
