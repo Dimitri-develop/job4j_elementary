b0bedffa413e0e8cf03a4ebf3af959253cd2b0bc
dima- <dmitry.yamanko@gmail.com>
2021-03-11 22:44:45 +0100

    6. Идеальный вес [#489575]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import java.util.Scanner;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+
+    public static void main(String[] args) {
+        //short height = 187;//cm
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your height in cm: " );
+        short nextShort = scanner.nextShort();
+        double manWeight = Fit.manWeight(nextShort);
+        double womanWeight = Fit.womanWeight(nextShort);
+        System.out.println("Man " + nextShort + " is " + manWeight );
+        System.out.println("Woman " + nextShort + " is " + womanWeight );
+    }
+}
