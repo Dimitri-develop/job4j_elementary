5a728d071ed3f418ec17e2cd448120c201eecda4
dima- <dmitry.yamanko@gmail.com>
2021-03-24 19:22:54 +0100

    4.1.3. Блок-схемы [#489526]

---
+++ src/ru/job4j/condition/MaxOfThreeNum.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class MaxOfThreeNum {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        if (x > y && y > z) {
+            System.out.println("Max of three num is : " + x);
+        } else if (y > x && y > z) {
+            System.out.println("Max of three num is : " + y);
+        } else if (z > x && z > y) {
+            System.out.println("Max of three num is : " + z);
+        }
+    }
+}
