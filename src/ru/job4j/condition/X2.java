a0275eb96ee87cfabadb4c1e94ee0e1cc1a3bb5f
dima- <dmitry.yamanko@gmail.com>
2021-03-24 13:05:10 +0100

    1.1. Входные данные в тесте [#489613]

---
+++ src/ru/job4j/condition/X2.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+
+public class X2 {
+
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        //a = 1, b = 1, c = 1, x = 1
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 1;
+        /*Ожидаемое значение. */
+        int expected = 3;
+        /*Результат вычисления. */
+        int rsl = X2.calc(a, b, c, x);
+        /*Сравнение полученного значения с ожидаемым. */
+        Assert.assertEquals(expected, rsl);
+    }
+
+}
